442db9d86b238ac1b54f04e94311dddb8fa8a68a
Vitaliy Stepanyuk <vist98@gmail.com>
2026-02-21 16:50:43 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,14 +1,29 @@
 class Animal:
-    alive = []
+    alive: list["Animal"] = []
 
-    def __init__(self,
-                 name: str,
-                 health: int = 100,
-                 hidden: bool = False) -> None:
+    def __init__(
+            self,
+            name: str,
+            health: int = 100,
+            hidden: bool = False
+    ) -> None:
         self.name = name
         self.health = health
         self.hidden = hidden
         Animal.alive.append(self)
+
+    @property
+    def health(self) -> int:
+        return self._health
+
+    @health.setter
+    def health(
+            self,
+            value: int
+    ) -> None:
+        self._health = value
+        if self._health <= 0 and self in Animal.alive:
+            Animal.alive.remove(self)
 
     def __repr__(self) -> str:
         return (f"{{Name: {self.name}, "
@@ -22,8 +37,9 @@ class Herbivore(Animal):
 
 
 class Carnivore(Animal):
-    def bite(self, animal: Animal) -> None:
+    def bite(
+            self,
+            animal: Herbivore
+    ) -> None:
         if isinstance(animal, Herbivore) and not animal.hidden:
             animal.health -= 50
-            if animal.health <= 0 and animal in Animal.alive:
-                animal.alive.remove(animal)
